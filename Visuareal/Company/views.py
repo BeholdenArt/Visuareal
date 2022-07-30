41148714afe09ff934cf167dab179c3e03b9fbf8
beholdenart <priyanshub5645@gmail.com>
2022-07-30 15:40:34 +0530

    Company Integration ADD and DELETE functionality remains + dashboard's landing page (home page)

--- Visuareal/Company/views.py
+++ Visuareal/Company/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from Company.models import AddCustomer, CompanyInventory, OrderQueue, AddCompany
+from Dealer.models import AddDealer
+from Influencer.models import AddInfluencer
+
 # Create your views here.
 
-
 def home(request):
-	return HttpResponse("Company Views")
+	return render(request, 'base.html')
+
+def customerList(request):
+
+	contents = AddCustomer.objects.all()
+	# print(contents[0].interestedProduct.all())
+	return render(request, 'customerlist.html', {'all_data' : contents})
+
+def companyInventory(request):
+	contents = CompanyInventory.objects.all()
+	return render(request, 'inventorylist.html', {'all_data' : contents})
+
+def dealerList(request):
+	contents = AddDealer.objects.all()
+	return render(request, 'dealerlist.html', {'all_data' : contents}) 
+
+def influencerList(request):
+	contents = AddInfluencer.objects.all()
+	return render(request, 'influencerlist.html', {'all_data' : contents})
+
+def orderQueue(request):
+	contents = OrderQueue.objects.all()
+	return render(request, 'orderqueue.html', {'all_data' : contents})
